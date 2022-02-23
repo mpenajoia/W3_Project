cff791ddc77c21e9680d808ba48f1f9fdf66449e
Marco Penajoia <penajoia@MPenajoias-MBP.local>
2022-02-22 16:52:25 -0800

    gifs used with api and useFetch

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -3,8 +3,12 @@ import { TransactionContext } from "../context/TransactionContext";
 
 import dummyData from '../utils/dummyData';
 import { shortenAddress } from "../utils/shortenAddress";
+import useFetch from "../hooks/useFetch";
+
 
 const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
+    const gifUrl = useFetch({ keyword })
+    
     return (
         <div className="bg-[#181918] m-4 flex flex-1 
             2xl:min-w-[450px]
@@ -37,12 +41,20 @@ const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, 
                         </>
                     )}
 
-                    <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
-                        <p className="text-[#37c7da] font-bold">
-                            {timestamp}
-                        </p>
 
-                    </div>
+                </div>
+                <img 
+                    src={gifUrl || url}
+                    alt="gif"
+                    className="w-full h-64 2x:h-96 rounded-md shadow-lg object-cover"
+                />
+
+
+                <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+                    <p className="text-[#37c7da] font-bold">
+                        {timestamp}
+                    </p>
+
                 </div>
             </div>
         </div>
@@ -61,7 +73,7 @@ const Transactions = () => {
                 )}
                 <div className="flex flex-wrap justify-center items-center mt-10">
                     {dummyData.reverse().map((transaction, i) => (
-                        <TransactionCard key={1} {...transaction} />
+                        <TransactionCard key={i} {...transaction} />
                     ))}
                 </div>
             </div>
